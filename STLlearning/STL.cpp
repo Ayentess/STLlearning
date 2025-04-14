5d247d2bde8198e02105a59bd35921454601fd7e
Ayentess <2479277792@qq.com>
2025-04-14 22:03:43 +0800

    STL

--- STLlearning/STL.cpp
+++ STLlearning/STL.cpp
@@ -2407,68 +2407,553 @@
 //	return 0;
 //}
 
-#include<iostream>
-using namespace std;
-#include<string>
-#include<vector>
-#include<algorithm>
+//#include<iostream>
+//using namespace std;
+//#include<string>
+//#include<vector>
+//#include<algorithm>
+//
+//class Person
+//{
+//	friend void test01();
+//public:
+//	//friend void test01();
+//	Person(string name, int age)
+//	{
+//		this->m_name = name;
+//		this->m_age = age;
+//	}
+//
+//	bool operator==(const Person& p)
+//	{
+//		if (this->m_age == p.m_age && this->m_name == p.m_name)
+//		{
+//			return true;
+//		}
+//		else
+//		{
+//			return false;
+//		}
+//	}
+//
+//private:
+//	string m_name;
+//	int m_age;
+//};
+//
+//void test01()
+//{
+//	vector <Person> v;
+//
+//	Person p1("aaa", 10);
+//    Person p2("bbb", 20);
+//    Person p3("ccc", 30);
+//    Person p4("ddd", 40);
+//
+//	v.push_back(p1);
+//	v.push_back(p2);
+//	v.push_back(p3);
+//	v.push_back(p4);
+//
+//	vector<Person>::iterator it =find(v.begin(), v.end(), p2);
+//
+//	if (it == v.end())
+//	{
+//        cout << "没有找到!" << endl;
+//	}
+//	else
+//	{
+//        cout << "找到姓名:" << it->m_name << " 年龄: " << it->m_age << endl;
+//	}
+//}
+//
+//int main() 
+//{
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-class Person
-{
-	friend void test01();
-public:
-	//friend void test01();
-	Person(string name, int age)
-	{
-		this->m_name = name;
-		this->m_age = age;
-	}
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//#include <string>
+//
+////内置数据类型
+//class GreaterFive
+//{
+//public:
+//	bool operator()(int val)
+//	{
+//		return val > 5;
+//	}
+//};
+//
+//void test01() {
+//
+//	vector<int> v;
+//	for (int i = 0; i < 10; i++) {
+//		v.push_back(i + 1);
+//	}
+//
+//	vector<int>::iterator it = find_if(v.begin(), v.end(), GreaterFive());
+//	if (it == v.end()) {
+//		cout << "没有找到!" << endl;
+//	}
+//	else {
+//		cout << "找到大于5的数字:" << *it << endl;
+//	}
+//}
+//
+////自定义数据类型
+//class Person {
+//public:
+//	Person(string name, int age)
+//	{
+//		this->m_Name = name;
+//		this->m_Age = age;
+//	}
+//public:
+//	string m_Name;
+//	int m_Age;
+//};
+//
+//class Greater20
+//{
+//public:
+//	bool operator()(Person& p)
+//	{
+//		return p.m_Age > 20;
+//	}
+//
+//};
+//
+//void test02() {
+//
+//	vector<Person> v;
+//
+//	//创建数据
+//	Person p1("aaa", 10);
+//	Person p2("bbb", 20);
+//	Person p3("ccc", 30);
+//	Person p4("ddd", 40);
+//
+//	v.push_back(p1);
+//	v.push_back(p2);
+//	v.push_back(p3);
+//	v.push_back(p4);
+//
+//	vector<Person>::iterator it = find_if(v.begin(), v.end(), Greater20());
+//	if (it == v.end())
+//	{
+//		cout << "没有找到!" << endl;
+//	}
+//	else
+//	{
+//		cout << "找到姓名:" << it->m_Name << " 年龄: " << it->m_Age << endl;
+//	}
+//}
+//
+//int main() {
+//
+//	//test01();
+//
+//	test02();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	bool operator==(const Person& p)
-	{
-		if (this->m_age == p.m_age && this->m_name == p.m_name)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+//找到相邻重复的元素
+//#include <iostream>
+//using namespace std;
+//#include<algorithm>
+//#include<vector>
+//
+//int main() 
+//{
+//	vector<int> v;
+//
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(3);
+//	v.push_back(4);
+//	v.push_back(5);
+//
+//	vector<int>::iterator ops = adjacent_find(v.begin(), v.end());
+//
+//	if (ops == v.end())
+//	{
+//        cout << "没有找到!" << endl;
+//	}
+//	else
+//	{
+//        cout << "找到相邻重复的元素:" << *ops << endl;
+//	}
+//	system("pause");
+//
+//	return 0;
+//}
 
-private:
-	string m_name;
-	int m_age;
-};
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//void test01()
+//{
+//	vector<int>v;
+//
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v.push_back(i);
+//	}
+//	//二分查找
+//	bool ret = binary_search(v.begin(), v.end(), 2);
+//	if (ret)
+//	{
+//		cout << "找到了" << endl;
+//	}
+//	else
+//	{
+//		cout << "未找到" << endl;
+//	}
+//}
+//
+//int main() 
+//{
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-void test01()
-{
-	vector <Person> v;
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+////内置数据类型
+//void test01()
+//{
+//	vector<int> v;
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(4);
+//	v.push_back(5);
+//	v.push_back(3);
+//	v.push_back(4);
+//	v.push_back(4);
+//
+//	int num = count(v.begin(), v.end(), 4);
+//
+//	cout << "4的个数为： " << num << endl;
+//}
+//
+////自定义数据类型
+//class Person
+//{
+//public:
+//	Person(string name, int age)
+//	{
+//		this->m_Name = name;
+//		this->m_Age = age;
+//	}
+//	bool operator==(const Person& p)//重载==加const
+//	{
+//		if (this->m_Age == p.m_Age)
+//		{
+//			return true;
+//		}
+//		else
+//		{
+//			return false;
+//		}
+//	}
+//	string m_Name;
+//	int m_Age;
+//};
+//
+//void test02()
+//{
+//	vector<Person> v;
+//
+//	Person p1("刘备", 35);
+//	Person p2("关羽", 35);
+//	Person p3("张飞", 35);
+//	Person p4("赵云", 30);
+//	Person p5("曹操", 25);
+//
+//	v.push_back(p1);
+//	v.push_back(p2);
+//	v.push_back(p3);
+//	v.push_back(p4);
+//	v.push_back(p5);
+//
+//	Person p("诸葛亮", 35);
+//
+//	int num = count(v.begin(), v.end(), p);
+//	cout << "num = " << num << endl;
+//}
+//int main() {
+//
+//	//test01();
+//
+//	test02();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	Person p1("aaa", 10);
-    Person p2("bbb", 20);
-    Person p3("ccc", 30);
-    Person p4("ddd", 40);
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//class Greater4
+//{
+//public:
+//	bool operator()(int val)
+//	{
+//		return val >= 4;
+//	}
+//};
+//
+////内置数据类型
+//void test01()
+//{
+//	vector<int> v;
+//	v.push_back(1);
+//	v.push_back(2);
+//	v.push_back(4);
+//	v.push_back(5);
+//	v.push_back(3);
+//	v.push_back(4);
+//	v.push_back(4);
+//
+//	int num = count_if(v.begin(), v.end(), Greater4());
+//
+//	cout << "大于4的个数为： " << num << endl;
+//}
+//
+////自定义数据类型
+//class Person
+//{
+//public:
+//	Person(string name, int age)
+//	{
+//		this->m_Name = name;
+//		this->m_Age = age;
+//	}
+//
+//	string m_Name;
+//	int m_Age;
+//};
+//
+//class AgeLess35
+//{
+//public:
+//	bool operator()(const Person& p)
+//	{
+//		return p.m_Age < 35;
+//	}
+//};
+//void test02()
+//{
+//	vector<Person> v;
+//
+//	Person p1("刘备", 35);
+//	Person p2("关羽", 35);
+//	Person p3("张飞", 35);
+//	Person p4("赵云", 30);
+//	Person p5("曹操", 25);
+//
+//	v.push_back(p1);
+//	v.push_back(p2);
+//	v.push_back(p3);
+//	v.push_back(p4);
+//	v.push_back(p5);
+//
+//	int num = count_if(v.begin(), v.end(), AgeLess35());
+//	cout << "小于35岁的个数：" << num << endl;
+//}
+//
+//
+//int main() {
+//
+//	//test01();
+//
+//	test02();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	v.push_back(p1);
-	v.push_back(p2);
-	v.push_back(p3);
-	v.push_back(p4);
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//void myPrint(int val)
+//{
+//	cout << val << " ";
+//}
+//
+//void test01() {
+//	vector<int> v;
+//	v.push_back(10);
+//	v.push_back(30);
+//	v.push_back(50);
+//	v.push_back(20);
+//	v.push_back(40);
+//
+//	//sort默认从小到大排序
+//	sort(v.begin(), v.end());
+//	for_each(v.begin(), v.end(), myPrint);
+//	cout << endl;
+//
+//	//从大到小排序
+//	sort(v.begin(), v.end(), greater<int>());
+//	for_each(v.begin(), v.end(), myPrint);
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	vector<Person>::iterator it =find(v.begin(), v.end(), p2);
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//#include <ctime>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	srand(time(NULL));
+//	vector<int> v;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v.push_back(i);
+//	}
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//
+//	//打乱顺序
+//	random_shuffle(v.begin(), v.end());
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	if (it == v.end())
-	{
-        cout << "没有找到!" << endl;
-	}
-	else
-	{
-        cout << "找到姓名:" << it->m_name << " 年龄: " << it->m_age << endl;
-	}
-}
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v1;
+//	vector<int> v2;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v1.push_back(i);
+//		v2.push_back(i + 1);
+//	}
+//
+//	vector<int> vtarget;
+//	//目标容器需要提前开辟空间
+//	vtarget.resize(v1.size() + v2.size());
+//	//合并  需要两个有序序列
+//	merge(v1.begin(), v1.end(), v2.begin(), v2.end(), vtarget.begin());
+//	for_each(vtarget.begin(), vtarget.end(), myPrint());
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-int main() 
-{
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v;
+//	v.push_back(10);
+//	v.push_back(30);
+//	v.push_back(50);
+//	v.push_back(20);
+//	v.push_back(40);
+//
+//	cout << "反转前： " << endl;
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//
+//	cout << "反转后： " << endl;
+//
+//	reverse(v.begin(), v.end());
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//}
+
+int main() {
+
 	test01();
 
 	system("pause");
