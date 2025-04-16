eb94d3974d2c0bd2c08a557480250c5dc19b2e6f
Ayentess <2479277792@qq.com>
2025-04-16 20:37:28 +0800

    STL

--- STLlearning/STL.cpp
+++ STLlearning/STL.cpp
@@ -2952,11 +2952,395 @@
 //	cout << endl;
 //}
 
-int main() {
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v1;
+//	for (int i = 0; i < 10; i++) 
+//	{
+//		v1.push_back(i + 1);
+//	}
+//	vector<int> v2;
+//	v2.resize(v1.size());//提前开辟空间
+//	copy(v1.begin(), v1.end(), v2.begin());
+//
+//	for_each(v2.begin(), v2.end(), myPrint());
+//	cout << endl;
+//}
+//
+//int main() 
+//{
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	test01();
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v;
+//	v.push_back(20);
+//	v.push_back(30);
+//	v.push_back(20);
+//	v.push_back(40);
+//	v.push_back(50);
+//	v.push_back(10);
+//	v.push_back(20);
+//
+//	cout << "替换前：" << endl;
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//
+//	//将容器中的20 替换成 2000
+//	cout << "替换后：" << endl;
+//	replace(v.begin(), v.end(), 20, 2000);
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	system("pause");
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//class ReplaceGreater30
+//{
+//public:
+//	bool operator()(int val)
+//	{
+//		return val >= 30;
+//	}
+//
+//};
+//
+//void test01()
+//{
+//	vector<int> v;
+//	v.push_back(20);
+//	v.push_back(30);
+//	v.push_back(20);
+//	v.push_back(40);
+//	v.push_back(50);
+//	v.push_back(10);
+//	v.push_back(20);
+//
+//	cout << "替换前：" << endl;
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//
+//	//将容器中大于等于的30 替换成 3000
+//	cout << "替换后：" << endl;
+//	replace_if(v.begin(), v.end(), ReplaceGreater30(), 3000);
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//}
+//
+//int main() 
+//{
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//#include <algorithm>
+//#include <vector>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v1;
+//	vector<int> v2;
+//	for (int i = 0; i < 10; i++) {
+//		v1.push_back(i);
+//		v2.push_back(i + 100);
+//	}
+//
+//	cout << "交换前： " << endl;
+//	for_each(v1.begin(), v1.end(), myPrint());
+//	cout << endl;
+//	for_each(v2.begin(), v2.end(), myPrint());
+//	cout << endl;
+//
+//	cout << "交换后： " << endl;
+//	swap(v1, v2);
+//	for_each(v1.begin(), v1.end(), myPrint());
+//	cout << endl;
+//	for_each(v2.begin(), v2.end(), myPrint());
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//#include <numeric>
+//#include <vector>
+//void test01()
+//{
+//	vector<int> v;
+//	for (int i = 0; i <= 100; i++) 
+//	{
+//		v.push_back(i);
+//	}
+//
+//	int total = accumulate(v.begin(), v.end(), 1);
+//
+//	cout << "total = " << total << endl;
+//}
+//
+//int main() 
+//{
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//#include <numeric>
+//#include <vector>
+//#include <algorithm>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//
+//	vector<int> v;
+//	v.resize(10);
+//	//填充
+//	fill(v.begin(), v.end(), 100);
+//
+//	for_each(v.begin(), v.end(), myPrint());
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+//求交集
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <algorithm>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v1;
+//	vector<int> v2;
+//	for (int i = 0; i < 10; i++)
+//	{
+//		v1.push_back(i);
+//		v2.push_back(i + 5);
+//	}
+//
+//	vector<int> vTarget;
+//	//取两个里面较小的值给目标容器开辟空间
+//	vTarget.resize(min(v1.size(), v2.size()));
+//
+//	//返回目标容器的最后一个元素的迭代器地址
+//	vector<int>::iterator itEnd =
+//		set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), vTarget.begin());
+//
+//	for_each(vTarget.begin(), itEnd, myPrint());
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+//求并集
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <algorithm>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v1;
+//	vector<int> v2;
+//	for (int i = 0; i < 10; i++) {
+//		v1.push_back(i);
+//		v2.push_back(i + 5);
+//	}
+//
+//	vector<int> vTarget;
+//	//取两个容器的和给目标容器开辟空间
+//	vTarget.resize(v1.size() + v2.size());
+//
+//	//返回目标容器的最后一个元素的迭代器地址
+//	vector<int>::iterator itEnd =
+//		set_union(v1.begin(), v1.end(), v2.begin(), v2.end(), vTarget.begin());
+//
+//	for_each(vTarget.begin(), itEnd, myPrint());
+//	cout << endl;
+//}
+//
+//int main() {
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
+
+//求差集
+//#include <iostream>
+//using namespace std;
+//#include <vector>
+//#include <algorithm>
+//
+//class myPrint
+//{
+//public:
+//	void operator()(int val)
+//	{
+//		cout << val << " ";
+//	}
+//};
+//
+//void test01()
+//{
+//	vector<int> v1;
+//	vector<int> v2;
+//	for (int i = 0; i < 10; i++) 
+//	{
+//		v1.push_back(i);
+//		v2.push_back(i + 5);
+//	}
+//
+//	vector<int> vTarget;
+//	//取两个里面较大的值给目标容器开辟空间
+//	vTarget.resize(max(v1.size(), v2.size()));
+//
+//	//返回目标容器的最后一个元素的迭代器地址
+//	cout << "v1与v2的差集为： " << endl;
+//	vector<int>::iterator itEnd =
+//		set_difference(v1.begin(), v1.end(), v2.begin(), v2.end(), vTarget.begin());
+//	for_each(vTarget.begin(), itEnd, myPrint());
+//	cout << endl;
+//
+//
+//	cout << "v2与v1的差集为： " << endl;
+//	itEnd = set_difference(v2.begin(), v2.end(), v1.begin(), v1.end(), vTarget.begin());
+//	for_each(vTarget.begin(), itEnd, myPrint());
+//	cout << endl;
+//}
+//
+//int main() 
+//{
+//
+//	test01();
+//
+//	system("pause");
+//
+//	return 0;
+//}
